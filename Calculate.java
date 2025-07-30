7ad5a193cdf68ff1cb564b40b6cd0831f9e3630c
Utkarsh Rajoriya <utkarshrajoriya00@gmail.com>
2025-07-30 12:39:47 +0530

    calculate.java is added

---
+++ Calculate.java
@@ -0,0 +1,8 @@
+
+public class Calculate {
+    public static void main(String[] args) {
+        System.out.println("This is Calculate file running");
+        int result = Main.add(5, 8);
+        System.out.println("The sum is: " + result);
+    }
+}
--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main{
     }   
 
     public static void main(String[] args) {
-        System.out.println("This is Sum file running");
+        System.out.println("This is Main file running");
         int result = add(5, 3);
         System.out.println("The sum is: " + result);
     }

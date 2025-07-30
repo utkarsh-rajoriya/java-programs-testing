a705571ebbd02c88bcc9237bb9c61964156d63ce
Utkarsh Rajoriya <utkarshrajoriya00@gmail.com>
2025-07-30 12:31:26 +0530

    Main.java is added

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+
+    public static int add(int a, int b) {
+        return a + b;
+    }   
+
+    public static void main(String[] args) {
+        System.out.println("This is Sum file running");
+        int result = add(5, 3);
+        System.out.println("The sum is: " + result);
+    }
+}
